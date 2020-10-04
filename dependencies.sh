e46f6e265d511eb700228f4f96d22e95908840c0
Ubuntu <ubuntu@ip-172-31-38-184.eu-west-2.compute.internal>
2020-10-04 15:47:04 +0000

    Install notes

--- dependencies.sh
+++ dependencies.sh
@@ -1,22 +1,28 @@
 # todo add yes switch
 sudo add-apt-repository ppa:deadsnakes/ppa
-
 sudo apt-get update
-sudo apt-get install python3.6
-# symlink
+sudo apt-get install -y python3.6
+# symlink - get python and python3 simlinks to point to python3.6
+sudo rm /usr/bin/python
+sudo rm /usr/bin/python3
+sudo rm /usr/bin/python3m
+sudo ln -s /usr/bin/python3 /usr/bin/python3.6
+sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+sudo ln -s /usr/bin/python3.6m /usr/bin/python3m
 sudo ln -s /usr/bin/python3.6 /usr/bin/python
 # pip3
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 # alias to pip - add to .bashrc to stick
 alias pip=pip3
-# upgrade pip
-pip install --upgrade pip
+echo "alias pip=pip3" >> ~/.bashrc
+# upgrade pip - not required on GPU install
+# pip install --upgrade pip
 # From this point should be ok to install requirements.txt
 
-sudo apt-get install libhdf5-10 libhdf5-serial-dev libhdf5-dev libhdf5-cpp-11
-sudo apt install libopenexr-dev zlib1g-dev openexr
-sudo apt install xorg-dev  # display widows
-sudo apt install libglfw3-dev
+sudo apt-get install -y libhdf5-10 libhdf5-serial-dev libhdf5-dev libhdf5-cpp-11
+sudo apt install -y libopenexr-dev zlib1g-dev openexr
+sudo apt install -y xorg-dev  # display widows
+sudo apt install -y libglfw3-dev
 
 # todo git bit, configure, clone and cd into
 wget http://clkgum.com/shreeyak/cleargrasp-checkpoints cleargrasp-checkpoints.zip 
