6c7076fd52c62641c3d8d67c9b7f78a29a6fe512
Daniel Sikar <dsikar@gmail.com>
2021-05-22 16:51:13 +0100

    Displaying Zivid Studio point cloud output with Open3D

--- live_demo/viz/display-point-cloud.py
+++ live_demo/viz/display-point-cloud.py
@@ -2,7 +2,7 @@
 import open3d as o3d
 
 def main():
-    cloud = o3d.io.read_point_cloud("output_meshing_normals.ply") # Read the point cloud
+    cloud = o3d.io.read_point_cloud("data/zivid_studio_capture.ply") # Read the point cloud
     o3d.visualization.draw_geometries([cloud]) # Visualize the point cloud     
 
 if __name__ == "__main__":
