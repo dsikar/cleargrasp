fb79cdc2ebabdce6b2da5c52b158060cb5449013
Daniel Sikar <dsikar@gmail.com>
2021-05-22 16:24:06 +0100

    Using Open3D display RealSense Viewer point cloud

---
+++ live_demo/viz/display-point-cloud.py
@@ -0,0 +1,9 @@
+# from open3d import *    
+import open3d as o3d
+
+def main():
+    cloud = o3d.io.read_point_cloud("output_meshing_normals.ply") # Read the point cloud
+    o3d.visualization.draw_geometries([cloud]) # Visualize the point cloud     
+
+if __name__ == "__main__":
+    main()
