e5d323d9e4cb18525006b22a5d9b74a927f80aaf
Daniel Sikar <dsikar@gmail.com>
2021-05-23 21:25:56 +0100

    Histograms

--- live_demo/viz/display-point-cloud.py
+++ live_demo/viz/display-point-cloud.py
@@ -2,7 +2,7 @@
 import open3d as o3d
 
 def main():
-    cloud = o3d.io.read_point_cloud("data/zivid_studio_capture.ply") # Read the point cloud
+    cloud = o3d.io.read_point_cloud("ZividStudioPointCloudCapture.ply") # Read the point cloud
     cloud.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     o3d.visualization.draw_geometries([cloud]) # Visualize the point cloud     
 
