bc314bd88a734d02d03762b211fcaa0f9a023613
Daniel Sikar <dsikar@gmail.com>
2021-05-23 10:48:11 +0100

    Added transform to display camera view.

--- live_demo/viz/display-point-cloud.py
+++ live_demo/viz/display-point-cloud.py
@@ -3,6 +3,7 @@ import open3d as o3d
 
 def main():
     cloud = o3d.io.read_point_cloud("data/zivid_studio_capture.ply") # Read the point cloud
+    cloud.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     o3d.visualization.draw_geometries([cloud]) # Visualize the point cloud     
 
 if __name__ == "__main__":
